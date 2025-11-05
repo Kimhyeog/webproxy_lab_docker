e00824bcf94e3699e8784042aa04fc6b25e9e43f
김형준 <>
2025-11-05 11:15:59 +0900

    proxy 기본세팅

--- webproxy-lab/proxy.c
+++ webproxy-lab/proxy.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include "csapp.h"
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
@@ -9,8 +8,32 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
-int main()
+void doit(int connfd);
+
+int main(int argc, char **argv)
 {
-  printf("%s", user_agent_hdr);
+  int listenfd, connfd;
+  socklen_t clientlen;
+  struct sockaddr_storage clientaddr;
+  // 1. 터미널 명령어 오류 처리
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  // 1. 입력한 Port로 listenfd 소켓 생성
+  listenfd = Open_listenfd(argv[1]);
+  // 2. while 로 connfd 생성 및 연결 대기
+  while (1)
+  {
+    // 3. connfd 생성 및 client 연결
+    clientlen = sizeof(clientaddr);
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    // TODO: implement doit(connfd)
+    // doit(connfd);
+    Close(connfd);
+  }
+
   return 0;
 }
